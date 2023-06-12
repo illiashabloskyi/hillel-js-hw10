e4117e90c399d6aae55b105074aa0ef6d815b82b
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-13 00:06:40 +0300

    update script.js

--- script.js
+++ script.js
@@ -8,3 +8,27 @@
 
 // У міру змін виводити вміст масиву на сторінку.
 
+const userInputArrayLength = parseInt(prompt(`Введіть довжину масива: `));
+
+if (userInputArrayLength === null || isNaN(userInputArrayLength) || userInputArrayLength === ''){
+  alert(`Щось пішло не так...`)
+}
+
+const arr = [];
+
+for(let i = 0; i < userInputArrayLength; i++){
+  const userInputArrayElements = prompt(`Введіть ${i}й елемент масива: `);
+  arr.push(userInputArrayElements);
+  // TODO: Видалення за допомогою метода delete
+  // if (i === 1  || i === 2 || i === 3){
+  //   delete arr[i];
+  // }
+  console.log(arr)
+}
+
+// console.log(arr)
+
+// TODO: Видалення за допомогою метода splice
+arr.splice(1, 3)
+console.log(arr)
+
