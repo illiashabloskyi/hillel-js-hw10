9828370e5594e5d1aee5a9716211c938e2900287
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-13 00:18:14 +0300

    update script.js

--- script.js
+++ script.js
@@ -4,8 +4,6 @@
 
 // Видалити елементи з масиву з 2 по 4 (включно!).
 
-
-
 // У міру змін виводити вміст масиву на сторінку.
 
 const userInputArrayLength = parseInt(prompt(`Введіть довжину масива: `));
@@ -19,16 +17,28 @@ const arr = [];
 for(let i = 0; i < userInputArrayLength; i++){
   const userInputArrayElements = prompt(`Введіть ${i}й елемент масива: `);
   arr.push(userInputArrayElements);
-  // TODO: Видалення за допомогою метода delete
-  // if (i === 1  || i === 2 || i === 3){
-  //   delete arr[i];
-  // }
   console.log(arr)
 }
+
+// TODO: Видалення за допомогою метода delete
+// arr.sort()
+// console.log("Відсортований масив:")
+// console.log(arr)
+
+// delete arr[1];
+// delete arr[2];
+// delete arr[3];
+
+// console.log("Масив з видаленими елементами:")
+// console.log(arr);
 
 // console.log(arr)
 
 // TODO: Видалення за допомогою метода splice
+arr.sort()
+console.log("Відсортований масив:")
+console.log(arr)
 arr.splice(1, 3)
+console.log("Масив з видаленими елементами:")
 console.log(arr)
 
